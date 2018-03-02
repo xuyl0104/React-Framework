0b2b97fffc1f5b6aaedd435bd32f69fb8bf66d00
xuyl0104 <xuyl0104@gmail.com>
2018-03-02 09:26:23 +0800

    添加Tab组件

---
+++ src/ComponentDetails/TabDetails.js
@@ -0,0 +1,97 @@
+import React, {Component} from 'react';
+import './ComponentDetails.css';
+import Header from '../components/header/header';
+import Row from '../components/row/row';
+import Container from '../components/container/container';
+import Content from '../components/content/content';
+import Button from '../components/button/button';
+import Icon from 'antd-mobile/lib/icon';
+import Tab from '../components/tab/tab';
+import 'antd-mobile/lib/icon/style/css';
+
+class Details extends Component {
+    constructor() {
+        super();
+        this.state = {
+            key: 2,
+            selected: 0
+        };
+    }
+
+
+    render() {
+        let tabDiv;
+        if(this.state.key === 2) {
+            tabDiv = 
+                <Tab tabs={['本日', '本周']} 
+                    selected={this.state.selected} callBackParent={this.changeTab.bind(this)}/>
+            ;
+        } else if(this.state.key === 3) {
+            tabDiv = 
+                <Tab tabs={['本日', '本周', '本月']} selected={this.state.selected} callBackParent={this.changeTab.bind(this)}/>
+            ;
+        } else if(this.state.key === 4) {
+            tabDiv = 
+                <Tab tabs={['本日', '本周', '本月', '本年']} selected={this.state.selected} callBackParent={this.changeTab.bind(this)}/>
+            ;
+        } else if(this.state.key === 5){
+            tabDiv = 
+                <Tab tabs={['本日', '本周', '本月', '本年', '所有']} selected={this.state.selected} callBackParent={this.changeTab.bind(this)}/>
+            ;
+        } else if(this.state.key === 6) {
+            tabDiv = 
+                <Tab tabs={['本日', '本周', '本月']} 
+                activeStyle={{color: 'red', backgroundColor: '#ffffff', fontWeight: 'bold'}}
+                inactiveStyle={{color: 'green', backgroundColor: '#ffffff'}}
+                indicatorStyle={{width: '2px', color: '#318ccf', style: 'dashed'}}
+                selected={this.state.selected} callBackParent={this.changeTab.bind(this)}/>
+            ;
+        }
+
+
+        return (
+            <div className="transition-item detail-page">
+                <Container>
+                    <Header name="Tab" 
+                        onLeftArrowClick={this.onLeftArrowClick.bind(this)}>
+                    </Header>
+                    {/* Tab */}
+                    {tabDiv}
+                    <Content>
+                        <div className="mt-3"></div>
+                        <Row>
+                            <Button style={"primary"} size="sm" text="2个tab" col={6} onClick={this.onButtonClick.bind(this, 2)}/>
+                            <Button style={"primary"} size="sm" text="3个tab" col={6} onClick={this.onButtonClick.bind(this, 3)}/>
+                            <Button style={"primary"} size="sm" text="4个tab" col={6} onClick={this.onButtonClick.bind(this, 4)}/>
+                            <Button style={"primary"} size="sm" text="5个tab" col={6} onClick={this.onButtonClick.bind(this, 5)}/>
+                            <Button style={"warning"} size="sm" text="tab样式" col={12} onClick={this.onButtonClick.bind(this, 6)}/>
+                        </Row> 
+                        <div style={{height: '2000px'}}></div> 
+                    </Content>
+                </Container>
+            </div>
+        );
+    }
+
+    changeTab(index) {
+        console.log("Tab clicked..." + index);
+        this.setState({
+            selected: index
+        });
+        // 此处添加点击不同Tab触发不同功能的代码
+        // ......
+    }
+
+    onButtonClick(key) {
+        this.setState({
+            key: key
+        });
+    }
+
+    onLeftArrowClick() {
+		this.props.history.goBack();
+    }
+    
+}
+
+export default Details;
---
+++ src/components/tab/tab.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+import './tab.css';
+import '../../CSS/style.css'; //可以引入CSS文件夹下的字体文件，替代图片图标，节省网络资源
+import Row from '../row/row';
+
+class Tab extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        let tabs = this.props.tabs;
+        let selectedTab = this.props.selected;
+        let numOfTabs = tabs.length;
+        let tabDivs;
+        let className;
+
+        let indicatorStyle = this.props.indicatorStyle || {color: '#318ccf', style: 'solid', width: '2px'};
+
+        let activeStyle = this.props.activeStyle || {color: '#318ccf', backgroundColor: '#ffffff'};
+        let activeIndicatorStyle = {
+            borderBottomColor: indicatorStyle.color || "#318ccf",
+            borderBottomStyle: indicatorStyle.style || "solid",
+            borderBottomWidth: indicatorStyle.width || "2px"
+        };
+
+        let allActiveStyle = {
+            ...activeStyle,
+            ...activeIndicatorStyle
+        };
+        let inactiveStyle = this.props.inactiveStyle || {color: '#000000', backgroundColor: '#ffffff'};
+
+        switch (numOfTabs) {
+            case 2:
+                className = "twoCols"
+                break;
+            case 3:
+                className = "threeCols"
+                break;
+            case 4:
+                className = "fourCols"
+                break;
+            case 5: // 三思啊
+                className = "fiveCols"
+                break;
+            default:
+                break;
+        }
+        tabDivs = tabs.map((item, index) => {
+            return (
+                <div className={className} 
+                    style={selectedTab===index?allActiveStyle:inactiveStyle}
+                    key={index} onClick={this.onTabChange.bind(this, index)}>{item}
+                </div>
+            );
+        });
+        return (
+            <div className="nav-tabs">
+                {tabDivs}
+            </div>
+        );
+    }
+
+    /**
+     * TODO: Tab下方的横线移动动画还未实现
+     * @param {*} tabIndex 
+     */
+    onTabChange(tabIndex) {
+        // let currentTab = this.state.selectedTab;
+        // let newTab = tabIndex;
+        // let tabAnimatationClass;
+        // if(newTab > currentTab) {
+        //     tabAnimatationClass = "no" + this.state.numOfTabs + "right" + (newTab - currentTab);
+        // } else {
+        //     tabAnimatationClass = "no" + this.state.numOfTabs + "left" + (newTab - currentTab);
+        // }
+
+        // console.log(currentTab + "--->" + tabIndex);
+        // console.log(tabAnimatationClass);
+        
+        let selectedTab = tabIndex;
+        // this.setState({
+        //     selectedTab: tabIndex,
+        //     // tabAnimatationClass: tabAnimatationClass
+        // });
+        this.props.callBackParent(selectedTab); 
+    }
+}
+  
+export default Tab
