cb7dcbfa77a068d5c11ff696516f6bd6cd957ebd
xuyl0104 <xuyl0104@gmail.com>
2018-03-02 17:05:44 +0800

    Input组件bug修复

--- src/ComponentDetails/InputDetails.js
+++ src/ComponentDetails/InputDetails.js
@@ -4,22 +4,22 @@ import Header from '../components/header/header';
 import Container from '../components/container/container';
 import Content from '../components/content/content';
 import Icon from 'antd/lib/icon';
+import 'antd/lib/icon/style/css';
 import Input from '../components/input/input';
 
 class Details extends Component {
     constructor() {
         super();
         this.state = {
-            text: "",
-            text2: ""
+            info: [{"a": "", "b": "", "c": "", "d": "", "e": "", "f": ""}]
         };
         this.onTextChange = this.onTextChange.bind(this);
-        this.onTextChange2 = this.onTextChange2.bind(this);
     }
 
     componentDidMount() {}
 
     render() {
+        console.log(this.state.info[0])
         return (
             <div className="transition-item detail-page">
                 <Container>
@@ -27,14 +27,14 @@ class Details extends Component {
                         onLeftArrowClick={this.onLeftArrowClick.bind(this)}>
                     </Header>
                     <Content padding={[0, 0, 0, 0]}>
-                        <Input label={"左对齐带清空"} text={this.state.text} onChange={this.onTextChange} placeholder={"姓名"} align={"left"} clear={true} />
-                        <Input label={"上级审批人"} text={this.state.text} onChange={this.onTextChange} placeholder={"上级审批人姓名"} align={"left"} />
-                        <Input label={"左对齐带图标"} text={this.state.text} onChange={this.onTextChange} placeholder={"审批意见"} align={"left"} 
+                        <Input label={"左对齐带清空"} name={"a"} text={this.state.info[0]["a"]} onChange={this.onTextChange} placeholder={"姓名"} align={"left"} clear={true} />
+                        <Input label={"上级审批人"} name={"b"} text={this.state.info[0]["b"]} onChange={this.onTextChange} placeholder={"上级审批人姓名"} align={"left"} />
+                        <Input label={"左对齐带图标"} name={"c"} text={this.state.info[0]["c"]} onChange={this.onTextChange} placeholder={"审批意见"} align={"left"} 
                             img={<Icon type="calendar" />}/>
-                        <Input label={"右对齐带清空"} text={this.state.text2} onChange={this.onTextChange2} placeholder={"请输入金额"} align={"right"} clear={true}/>
-                        <Input label={"交易金额"} text={this.state.text2} onChange={this.onTextChange2} placeholder={"请输入金额"} align={"right"} 
+                        <Input label={"右对齐带清空"} name={"d"} text={this.state.info[0]["d"]} onChange={this.onTextChange} placeholder={"请输入金额"} align={"right"} clear={true}/>
+                        <Input label={"交易金额"} name={"e"} text={this.state.info[0]["e"]} onChange={this.onTextChange} placeholder={"请输入金额"} align={"right"} 
                             img={<Icon type="right" />}/>
-                        <Input label={"交易金额"} text={this.state.text2} onChange={this.onTextChange2} placeholder={"请输入金额"} align={"right"} 
+                        <Input label={"交易金额"} name={"f"} text={this.state.info[0]["f"]} onChange={this.onTextChange} placeholder={"请输入金额"} align={"right"} 
                             img={<Icon type="pay-circle-o" />}/>
                     </Content>
                 </Container>
@@ -51,16 +51,10 @@ class Details extends Component {
     }
 
     onTextChange(e) {
-        let newValue = e.target.value;
-        // console.log(typeof newValue);
+        let info = this.state.info;
+        info[0][e.target.name] = e.target.value;
         this.setState({
-            text: newValue
-        });
-    }
-    onTextChange2(e) {
-        let newValue = e.target.value;
-        this.setState({
-            text2: newValue
+            info: info
         });
     }
 }
--- src/components/input/input.js
+++ src/components/input/input.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import './input.css';
-// import Icon from 'antd/lib/icon';
 import Icon from 'antd-mobile/lib/icon';
+import 'antd-mobile/lib/icon/style/css';
 
 class Input extends Component {
     constructor(props) {
@@ -11,8 +11,9 @@ class Input extends Component {
         };
     }
     render() {
+        let name = this.props.name;
         let label = this.props.label;
-        let text = this.props.text;
+        let text = this.props.text || "";
         let placeholder = this.props.placeholder || "";
         let align = this.props.align || "left";
         let image = this.props.img;
@@ -22,12 +23,12 @@ class Input extends Component {
                 onClick={this.props.onClick}>
                     <label className={"col-4 col-lg-3"}><nobr>{label}</nobr></label>
                     <div className={"col-8 col-lg-9 d-flex justify-content-between pr-0"}>
-                        <input type="text" value={text} onChange={this.onTextChange.bind(this)} placeholder={placeholder}
+                        <input type="text" value={text} onChange={this.onTextChange.bind(this)} placeholder={placeholder} name={name}
                             onFocus={this.focus.bind(this)} onBlur={this.blur.bind(this)}
                             style={{textAlign: `${align}`, border: 'none', width: '100%', fontSize: '17px'}}/>
                         <div className="pt-1 mt-2 ml-2" 
                             style={{display: (clear===true && text !== '' && this.state.showClear) ? '' : 'none'}} 
-                            onClick={this.clear.bind(this)}>
+                            onClick={this.clear.bind(this, name)}>
                             <Icon type="cross-circle-o" size="xs" color={"#aaa"}/>
                         </div>
                         <div className="pt-2 ml-2">{image}</div>
@@ -54,6 +55,7 @@ class Input extends Component {
     clear(e) {
         this.props.onChange(e = {
             target: {
+                name: this.props.name,
                 value: ""
             }
         });
