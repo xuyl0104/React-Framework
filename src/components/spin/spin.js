5a123214278a516947d29ffa3610995d998d4419
xuyl0104 <xuyl0104@gmail.com>
2018-03-04 16:51:49 +0800

    添加Spin组件

---
+++ src/components/spin/spin.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import './spin.css';
+import './css/fontello.css';
+import './css/animation.css';
+
+class Spin extends Component {
+    render() {
+        let isSpinning = this.props.isSpinning;
+        let indicator = this.props.indicator || "a";
+        let className;
+        switch (indicator) {
+            case "a":
+                className = "icon-spin4";
+                break;
+            case "b":
+                className = "icon-spin5";
+                break;
+            case "c":
+                className = "icon-spin6";
+                break;
+            default:
+                className = "icon-spin4";
+                break;
+        }
+        let indicatorSize = this.props.size || 30;
+        let indicatorColor = this.props.color || "#318ccf";
+        return (
+            <div className="spin" style={{display: isSpinning ? 'block' : 'none'}}>
+                <div className="theIcon">
+                    <i className={`${className} animate-spin`} 
+                        style={{fontSize: `${indicatorSize}px`, color: indicatorColor, marginLeft: `${-indicatorSize/2}px`}}>
+                    </i>
+                </div>
+            </div>
+        );
+    }
+}
+  
+export default Spin
